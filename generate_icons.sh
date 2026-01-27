4607642d5d6b387bbaf858a2f21ce23933c37545
gyanish0 <srivastavagyanish@gmail.com>
2026-01-27 20:15:09 +0530

    update logo

--- generate_icons.sh
+++ generate_icons.sh
@@ -41,21 +41,21 @@ sips -z 192 192 "$SOURCE_IMAGE" --out android/app/src/main/res/mipmap-xxxhdpi/ic
 echo "Generating iOS icons..."
 
 # iOS icons
-sips -z 20 20 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-20.png
-sips -z 40 40 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-20@2x.png
-sips -z 60 60 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-20@3x.png
-sips -z 29 29 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-29.png
-sips -z 58 58 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-29@2x.png
-sips -z 87 87 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-29@3x.png
-sips -z 40 40 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-40.png
-sips -z 80 80 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-40@2x.png
-sips -z 120 120 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-40@3x.png
-sips -z 120 120 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-60@2x.png
-sips -z 180 180 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-60@3x.png
-sips -z 76 76 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-76.png
-sips -z 152 152 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-76@2x.png
-sips -z 167 167 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-83.5@2x.png
-sips -z 1024 1024 "$SOURCE_IMAGE" --out ios/cars24x7userapp/Images.xcassets/AppIcon.appiconset/Icon-1024.png
+sips -z 20 20 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-20.png
+sips -z 40 40 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-20@2x.png
+sips -z 60 60 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-20@3x.png
+sips -z 29 29 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-29.png
+sips -z 58 58 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-29@2x.png
+sips -z 87 87 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-29@3x.png
+sips -z 40 40 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-40.png
+sips -z 80 80 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-40@2x.png
+sips -z 120 120 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-40@3x.png
+sips -z 120 120 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-60@2x.png
+sips -z 180 180 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-60@3x.png
+sips -z 76 76 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-76.png
+sips -z 152 152 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-76@2x.png
+sips -z 167 167 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-83.5@2x.png
+sips -z 1024 1024 "$SOURCE_IMAGE" --out ios/HomeDootPartner/Images.xcassets/AppIcon.appiconset/Icon-1024.png
 
 echo "✅ Icons generated successfully!"
 echo ""
--- ios/HomeDootPartnerTests/HomeDootPartnerTests.m
+++ ios/HomeDootPartnerTests/HomeDootPartnerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface cars24x7userappTests : XCTestCase
+@interface HomeDootPartnerTests : XCTestCase
 
 @end
 
-@implementation cars24x7userappTests
+@implementation HomeDootPartnerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
